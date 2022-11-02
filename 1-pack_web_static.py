e1471aef6f92498b4a58fe18cb8f3c0a8fe0a279
charlemagne <agbolossouc@gmail.com>
2022-11-02 17:09:14 +0000

    gen tgz using Fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Fabric script that generate a .tgz archive
+"""
+from fabric.api import *
+
+
+def do_pack():
+	"""
+	Generate the .tgz archive
+	"""
+    	from datetime import datetime
+    	import os.path
+
+    	date = datetime.now()
+
+    	tgz_file_path = 'versions/web_static_{}{}{}{}{}{}.tgz'.format(
+      		date.year, date.month, date.day, date.hour, date.minute, date.second)
+
+	if not os.path.exists('versions'):
+		os.mkdir('versions')
+
+	print('Packing web_static to ' + tgz_file_path)
+	res = local('tar  -cvzf {} {}'.format(tgz_file_path, 'web_static'))
+
+	if res.stderr:
+		return None
+
+	file_stats = os.stat(tgz_file_path)
+
+	print('web_static packed: {} -> {}Bytes'.format(
+		tgz_file_path, file_stats.st_size))
+
+    	if not res.stderr:
+		return os.path.realpath(tgz_file_path)
+	else:
+		return None
