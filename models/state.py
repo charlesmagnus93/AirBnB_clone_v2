f1cf684abd0f85d4126a1467a2895d86abd1c440
ch@rlemm@gne <agbolossouc@gmail.com>
2022-11-17 01:10:53 +0000

    improve engines

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128))
+    cities = relationship(
+            'City', back_populates='state',
+            cascade='all, delete, delete-orphan')
+
+
+if os.environ.get('HBNB_TYPE_STORAGE') != 'db':
+    def cities(self):
+        from models import storage
+        State.id = self.id
+        return storage.cities()
+
+    State.cities = property(lambda self: cities(self))
