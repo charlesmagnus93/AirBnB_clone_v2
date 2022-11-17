fa1bd4b4d56e5339411fcd4a0f21b32699e35e41
ch@rlemm@gne <agbolossouc@gmail.com>
2022-11-17 00:09:38 +0000

    flask app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Flask app
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.get('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
